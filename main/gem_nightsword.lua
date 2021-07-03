11aec5da8972a91d2f1b18c6ccba8e22919f05df
秋一 <civi@rcg.fun>
2021-07-04 01:04:32 +0800

    fix a temp bug.

--- main/gem_nightsword.lua
+++ main/gem_nightsword.lua
@@ -149,24 +149,24 @@ ENV.AddPrefabPostInit("nightsword", function(inst)
                     local gem = inv:FindItem(function(new_item) return new_item.prefab == item.prefab end)
                     if gem then
                         -- local slot_widget
-                        local slot = inv:GetItemSlot(gem)
-                        local overflow = inv:GetOverflowContainer()
-                        local controls = owner.HUD.controls
-                        local slot_widget = slot and controls.inv.inv[slot] -- check inventory first
-                            or ( inv:GetActiveItem() == gem and controls.inv.hovertile ) -- else, check active item
-                            or ( overflow -- else, if backpack, check backpack
-                                and (
-                                    controls.containers[overflow.inst] -- if backpack is side-display
-                                    and controls.containers[overflow.inst].inv[overflow:GetItemSlot(gem)]
-                                    or controls.inv.backpackinv[overflow:GetItemSlot(gem)]
-                                )
-                            )
+                        --local slot = inv:GetItemSlot(gem)
+                        --local overflow = inv:GetOverflowContainer()
+                        --local controls = owner.HUD.controls
+                        --local slot_widget = slot and controls.inv.inv[slot] -- check inventory first
+                        --    or ( inv:GetActiveItem() == gem and controls.inv.hovertile ) -- else, check active item
+                        --    or ( overflow -- else, if backpack, check backpack
+                        --        and (
+                        --            controls.containers[overflow.inst] -- if backpack is side-display
+                        --            and controls.containers[overflow.inst].inv[overflow:GetItemSlot(gem)]
+                        --            or controls.inv.backpackinv[overflow:GetItemSlot(gem)]
+                        --        )
+                        --    )
                         local single_gem = inv:RemoveItem(gem)
                         if single_gem then
-                            local pos = slot_widget
-                                and Vector3(TheSim:ProjectScreenPos(slot_widget:GetWorldPosition():Get()))
-                                or inst:GetPosition()
-                            container:GiveItem(single_gem, nil, pos)
+                            --local pos = slot_widget
+                            --    and Vector3(TheSim:ProjectScreenPos(slot_widget:GetWorldPosition():Get()))
+                            --    or inst:GetPosition()
+                            container:GiveItem(single_gem)
                         end
                     end
                 end
